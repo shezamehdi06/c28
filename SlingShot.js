d0faff6d49283d1ee3d3121186aee4ae1ddf7002
shezamehdi06 <64864204+shezamehdi06@users.noreply.github.com>
2020-07-25 12:38:57 -0500

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,33 @@
+class SlingShot{
+
+    constructor (bodyA,pointB){
+        var options={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.04,
+            length:10
+            
+        }
+        this.body=Constraint.create(options)
+
+        World.add(world,this.body);
+
+        
+    }
+
+    fly(){
+        this.body.bodyA=null;
+    }
+
+    display(){
+        if(this.body.bodyA){
+            var pointA=this.body.bodyA.position;
+            var pointB=this.body.pointB;
+            strokeWeight(4);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        }
+    }
+
+}
+
+          
